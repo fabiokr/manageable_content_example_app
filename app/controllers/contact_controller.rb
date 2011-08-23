633022f5d4d45d3e3735556caae80045b65098bd
Fabio Kreusch <fabiokr@gmail.com>
2011-08-23 12:00:33 -0700

    Added contents for website controllers

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,4 +1,7 @@
 class ContactController < ApplicationController
+
+  manageable_content_for :body, :side
+
   def index
   end
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
+
+  manageable_content_for :body
+
   def index
   end
 
