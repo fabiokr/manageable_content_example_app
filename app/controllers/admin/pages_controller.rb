4c6f599b9c9ec8b973e57cfd13638d8915f703d4
Fabio Kreusch <fabiokr@gmail.com>
2011-08-23 11:49:43 -0700

    Added Admin::PagesController logic

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,11 +1,26 @@
 class Admin::PagesController < Admin::ApplicationController
   def index
+    @pages = pages.order('key ASC, locale ASC')
   end
 
   def edit
+    @page = pages.find(params[:id])
   end
 
   def update
+    @page = pages.find(params[:id])
+
+    if @page.update_attributes(params[:manageable_content_page])
+      redirect_to admin_pages_path
+    else
+      render :edit
+    end
+  end
+
+  protected
+
+  def pages
+    ManageableContent::Manager.pages
   end
 
 end
